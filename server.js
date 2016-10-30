ac2db249200db529564e0b165888e2a232b82f19
BuildTools <unconfigured@null.spigotmc.org>
2016-10-30 08:35:50 -0500

    added-accounts

--- server.js
+++ server.js
@@ -13,17 +13,52 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
+var accountlist = {};
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
   
   ws.on('message', function(data, flags){
 	  console.log('Received: ' + data);
+	  
+	  var args = data.split(' ');
+	  if(args[0] == "@PLEXI"){
+		  console.log("Received @PLEXI message.");
+		  if(args[2] == "%Accounts%"){
+			  accounts(args, ws);
+		  }
+	  }
   });
 });
 
-setInterval(() => {
+function accounts(args, ws){
+	if(args[3] == "Ping"){
+		console.log("Received a ping!");
+		ws.send(args[4]);
+	}
+	
+	if(args[3] == "Create"){
+		accountlist[args[4]] = args[5];
+		console.log("Created account with Username: " + args[4] + " and Password: " + accountlist[args[4]]);
+		ws.send("@PLEXI %Accounts% %you% Success");
+	}
+	
+	if(args[3] == "Login"){
+		if(accountlist[args[4]] != "" && accountlist[args[4]] != undefined && accountlist[args[4]] != null){
+			if(accountlist[args[4]] == args[5]){
+				console.log("A user logged in as " + args[4]);
+				ws.send("@PLEXI %Accounts% %you% Success");
+			}
+			else{
+				ws.send("@PLEXI %Accounts% %you% Fail");
+			}
+		}
+	}
+}
+
+/*setInterval(() => {
   wss.clients.forEach((client) => {
     client.send(new Date().toTimeString());
   });
-}, 1000);
+}, 1000);*/
